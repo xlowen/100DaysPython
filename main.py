cd271ec783571d27f67f59bd1b0246666c5dd368
Amilton Siqueira <amiltonvsiqueira@gmail.com>
2024-10-18 12:35:11 -0300

    add collision to walls

--- main.py
+++ main.py
@@ -33,5 +33,10 @@ while game_is_on:
         food.refresh()
 
     #todo Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.game_over()
+        game_is_on = False
+
+
 
 screen.exitonclick()
